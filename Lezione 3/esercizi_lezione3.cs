80b843a597efe9537e22e9c4015806e1b05e9f18
Lorenzo Cassiani <lorenzo.cassiani@gmail.com>
2025-09-18 15:03:29 +0200

    aggiungo gli esercizi sulle Lezione 3

--- Lezione 3/esercizi_lezione3.cs
+++ Lezione 3/esercizi_lezione3.cs
@@ -1,36 +1,36 @@
-//ESERCIZI POMERIGGIO !!!! 
-using System.ComponentModel;
-using System.Security.Cryptography.X509Certificates;
-using System.Linq;
-using System.Collections.Generic;
+// //ESERCIZI POMERIGGIO !!!! 
+// using System.ComponentModel;
+// using System.Security.Cryptography.X509Certificates;
+// using System.Linq;
+// using System.Collections.Generic;
 
-namespace LezioneUno
-{
-    class Lezionetre
-    {
-        static void Main(string[] args)                
-        {
-            //esercizio 1
+// namespace LezioneUno
+// {
+//     class Lezionetre
+//     {
+//         static void Main(string[] args)                
+//         {
+//             //esercizio 1
 
-            List<int> listaNumerica = new List<int>(); //Creazione di una lista di interi
-            listaNumerica.Add(122); //Aggiunta di un elemento alla lista
+//             List<int> listaNumerica = new List<int>(); //Creazione di una lista di interi
+//             listaNumerica.Add(122); //Aggiunta di un elemento alla lista
 
-            //Console.WriteLine(listaNumerica[0]); //Stampa del primo elemento della lista
+//             //Console.WriteLine(listaNumerica[0]); //Stampa del primo elemento della lista
 
-            //esercizio 2
+//             //esercizio 2
 
-            List<string> listaNomi = new();
-            listaNomi.Add("anna");
-            listaNomi.Add("marco");
-            listaNomi.Add("luca");
-            foreach (string nome in listaNomi)
-            {
-                Console.WriteLine(nome.ToUpper());
-            }
+//             List<string> listaNomi = new();
+//             listaNomi.Add("anna");
+//             listaNomi.Add("marco");
+//             listaNomi.Add("luca");
+//             foreach (string nome in listaNomi)
+//             {
+//                 Console.WriteLine(nome.ToUpper());
+//             }
    
-            Console.ReadKey();
+//             Console.ReadKey();
 
-        }
-    }
-}
+//         }
+//     }
+// }
     
